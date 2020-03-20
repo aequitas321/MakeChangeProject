281bfb8bdf6d6bb4c6b5220897325570c5bb35fc
Shane Duncan <aequitas321@icloud.com>
2020-03-20 17:01:37 -0400

    Added to gitignore and first update to project

--- src/MakeChange/MakeChangeApp.java
+++ src/MakeChange/MakeChangeApp.java
@@ -1,8 +1,12 @@
 package MakeChange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("What is the price of the item? ");
 
 	}
 
