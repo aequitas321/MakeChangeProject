cfd3551777562b5bf960cccfec25b042d3133e41
Shane Duncan <aequitas321@icloud.com>
2020-03-20 23:58:40 -0400

    started work on method

--- src/MakeChange/MakeChangeApp.java
+++ src/MakeChange/MakeChangeApp.java
@@ -7,7 +7,24 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("What is the price of the item? ");
+		double cost = kb.nextDouble();
+		System.out.println("Amount of money recieved? ");
+		double tender = kb.nextDouble();
+		System.out.println(15 % 9);
+		changeCalc(cost, tender);
 
 	}
 
+	public static double changeCalc(double cost, double tender){
+		if(tender < cost){
+			System.out.println("Please provide correct amount.");
+		}else if(tender == cost){
+			System.out.println("Thank you for your purchase.");
+		}else if(tender > cost) {
+			double changeDue = tender - cost;
+			return changeDue;
+		}
+		return cost;
+
+	}
 }
