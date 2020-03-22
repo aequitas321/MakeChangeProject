6b8fd74195fced704ab0974418b43a215f52cce8
Shane Duncan <aequitas321@icloud.com>
2020-03-21 21:09:49 -0400

    Fixed the program where it only prints the amounts with a value

--- src/MakeChange/MakeChangeApp.java
+++ src/MakeChange/MakeChangeApp.java
@@ -10,11 +10,11 @@ public class MakeChangeApp {
 		double cost = kb.nextDouble();
 		System.out.println("Amount of money received? ");
 		double tender = kb.nextDouble();
-		System.out.println(changeCalc(cost, tender));
+		changeCalc(cost, tender);
 
 	}
 
-	public static String changeCalc(double cost, double tender) {
+	public static void changeCalc(double cost, double tender) {
 		double changeDue = 0;
 		int twenty = 0;
 		int ten = 0;
@@ -25,45 +25,69 @@ public class MakeChangeApp {
 		int nickle = 0;
 		int penny = 0;
 		if (tender == cost) {
-			return "Thank you for your purchase.";
+			System.out.println("Thank you for your purchase.");
 		} else if (tender > cost) {
+			System.out.println("Your change is: ");
 			changeDue = tender - cost;
 			while (changeDue >= 20) {
 				twenty++;
 				changeDue -= 20;
 			}
+			if(twenty > 0){
+				System.out.print(twenty + " twenties ");
+			}
 			while (changeDue >= 10) {
 				ten++;
 				changeDue -= 10;
+			}
+			if(ten > 0){
+				System.out.print(ten + " tens ");
 			}
 			while (changeDue >= 5) {
 				five++;
 				changeDue -= 5;
 			}
+			if(five > 0){
+				System.out.print(five + " five(s) ");
+			}
 			while (changeDue >= 1) {
 				dollar++;
 				changeDue -= 1;
+			}
+			if(dollar > 0){
+				System.out.print(dollar + " dollar(s) ");
 			}
 			while (changeDue >= .25) {
 				quarter++;
 				changeDue -= .25;
 			}
+			if(quarter > 0){
+				System.out.print(quarter + " quarters ");
+			}
 			while (changeDue >= .10) {
 				dime++;
 				changeDue -= .10;
+			}
+			if(dime > 0){
+				System.out.print(dime + " dime(s)");
 			}
 			while (changeDue >= .049) {
 				nickle++;
 				changeDue -= .05;
 			}
+			if(nickle > 0){
+				System.out.print(nickle + " nickle(s)");
+			}
 			while (changeDue >= .009) {
 				penny++;
 				changeDue -= .01;
 			}
-			return "Your change is " + twenty + " twenties, " + ten + " tens, " + five + " fives, " + dollar + " ones, " +
-					quarter + " quarters, " + dime + " dimes, " + nickle + " nickles " + penny + " pennies.";
+			if(penny > 0){
+				System.out.print(penny + " pennie(s)");
+			}
+
 		}else{
-			return "Please provide correct amount.";
+			System.out.println("Please provide correct amount.");
 		}
 
 
